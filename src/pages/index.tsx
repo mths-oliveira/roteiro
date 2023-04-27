531836a295ad0bdd2d9182e0ea7f82c263a3e340
Matheus Oliveira <contato.mths@outlook.com>
2023-04-27 19:11:22 -0300

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,56 +1,293 @@
-import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
+import { Heading, List, ListItem, Stack } from "@chakra-ui/react"
 
-import { Hero } from '../components/Hero'
-import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
-
-const Index = () => (
-  <Container height="100vh">
-    <Hero />
-    <Main>
-      <Text color="text">
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{' '}
-        <Code>TypeScript</Code>.
-      </Text>
-
-      <List spacing={3} my={0} color="text">
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
-
-    <DarkModeSwitch />
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
-    <CTA />
-  </Container>
-)
-
-export default Index
+export default function () {
+  return (
+    <Stack
+      padding={["2.5rem 1rem", "5rem 12.5rem"]}
+      spacing={["2.5rem", "5rem"]}
+      sx={{
+        ">section": {
+          ">h2": {
+            fontSize: "1.5rem",
+            marginBottom: "1.25rem",
+          },
+          ">ul>li:not(:first-of-type)": {
+            marginTop: "1rem",
+          },
+        },
+      }}
+    >
+      <section>
+        <Heading>DI</Heading>
+        <List>
+          <ListItem>
+            Então eu vou explicar tudo como funciona, mas antes posso fazer um
+            combinado com vc? [Aguarda a resposta]
+          </ListItem>
+          <ListItem>Legal!</ListItem>
+          <ListItem>
+            xxxx, eu tenho uma demanda muito grande de atendimento, eu atendo
+            mais de 100 países no nosso curso, então, pra eu conseguir dar conta
+            de atender todo mundo eu não retorno o contato. Minha ligação é
+            única, tudo bem? [Aguarda a resposta]
+          </ListItem>
+          <ListItem>
+            xxxx, eu vou explicar tudo como funciona, metodologia, financeiro,
+            horários, se qualquer coisa não encaixar pra vc, xxxx, não tem
+            problema nenhum. Eu quero q vc fique tranquila e a vontade pra me
+            falar não no final da nossa conversa. Combinado? [Aguarda a
+            resposta]
+          </ListItem>
+          <ListItem>
+            Por outro lado, xxxx, se você gostar do curso, perceber que a
+            metodologia te agrada, que encaixa nos seus horários e no seu bolso,
+            no final da ligação a gente já faz a sua matrícula. Combinado?
+            [Aguarda a resposta]
+          </ListItem>
+          <ListItem>
+            Para tomar essa decisão é você mesmo quem resolve? Vc não depende de
+            ninguém, né? [Aguarda a resposta]
+          </ListItem>
+        </List>
+      </section>
+      <section>
+        <Heading>Speech</Heading>
+        <List>
+          <ListItem>
+            xxxx, você conhece a Wise Up? Já ouviu falar? [Aguarda a resposta]
+          </ListItem>
+          <ListItem>
+            Só pra vc conhecer, a Wise Up hj tem a maior rede de ensino para
+            adultos da América Latina. A gente tem 28 anos de experiência, mais
+            de 400 escolas físicas espalhadas em todo o Brasil e hoje a gente
+            atende mais de 100 países, com 400 mil alunos.
+          </ListItem>
+          <ListItem>
+            O nosso curso é online, você tem acesso a um aplicativo, que é super
+            moderno, inspirado na Netflix e as aulas ao vivo.
+          </ListItem>
+          <ListItem>
+            As aulas ao vivo acontecem duas vezes por semana, duram 50
+            minutinhos e você pode escolher qualquer horário entre 7h da manhã e
+            11h da noite.
+          </ListItem>
+          <ListItem>
+            xxxx, de 7h da manhã a 11h da noite, você consegue encaixar 50
+            minutinhos, duas vezes na semana? [Aguarda a resposta]
+          </ListItem>
+          <ListItem>
+            Legal! Qual horário fica legal pra você? [Aguarda a resposta]
+          </ListItem>
+          <ListItem>
+            xxxx, vamos supor que você escolheu segunda e quarta às 7h da noite,
+            mas quarta-feira você teve um imprevisto e não conseguiu assistir a
+            aula. Não tem problema nenhum. Esse é um dos nossos maiores
+            diferenciais. Se você perder uma aula, é só entrar na plataforma e
+            remarcar, não tem custo a mais por isso. Então, você nunca perde
+            conteúdo com a gente. Legal?
+          </ListItem>
+          <ListItem>
+            Outra coisa legal é que se você quiser fazer aulas extras de
+            reforço, pra se desenvolver mais, você pode. Sem alterar em nada o
+            custo da sua mensalidade
+          </ListItem>
+          <ListItem>
+            Até aqui tranquilo? A questão dos horários e das aulas extras
+            encaixam pra você? Gostou dessa parte? [Aguarda a resposta]
+          </ListItem>
+          <ListItem>Legal!</ListItem>
+          <ListItem>
+            A nossa metodologia é focada na conversação, o nosso foco é que você
+            aprenda a se comunicar em inglês. Então você tem o contato direto
+            com o professor, são turmas bem reduzidas, então você tira dúvidas,
+            faz perguntas, e a gente vai te ensinar a pensar em inglês.
+          </ListItem>
+          <ListItem>
+            A gente vai trabalhar situações do dia a dia pra vc aprender a se
+            comunicar, pra vc conseguir se virar em uma viagem, a gente te
+            ensina também sobre o mercado de trabalho, pra vc crescer, se
+            desenvolver e buscar novas oportunidades.
+          </ListItem>
+          <ListItem>
+            E, xxxx, a Wise Up é uma escola bem diferente das escolas
+            tradicionais. As escolas tradicionais são excelentes, mas elas são
+            voltadas para um público diferente. Elas te preparam para a formação
+            acadêmica, então elas tem um excesso de gramática e você normalmente
+            fica de 5 a 7 anos para aprender o idioma.
+          </ListItem>
+          <ListItem>
+            A Wise Up é totalmente diferente, a gente vai te ensinar o inglês
+            funcional, usual e prático. O inglês que você utiliza no seu dia a
+            dia. A gente enxuga a gramática desnecessária que você não utiliza.
+            Por exemplo, xxxx, você sabe me definir o que é, mesóclise? [Aguarda
+            a resposta
+          </ListItem>
+          <ListItem>
+            Mesóclise é a colocação do pronome no meio do verbo, por exemplo.:
+            vê-lo-ei, contar-me-ás.
+          </ListItem>
+          <ListItem>
+            Muda alguma coisa na sua vida saber isso? Vc continua falando bem o
+            português? [Aguarda a resposta]
+          </ListItem>
+          <ListItem>
+            Essa é a ideia. Esse tipo de coisa que a gente enxuga.
+          </ListItem>
+          <ListItem>
+            E a gente vai te desenvolver para pensar em inglês. Você sabe me
+            dizer o que é yellow? [Aguarda a resposta]
+          </ListItem>
+          <ListItem>E o que é que é amarelo? [Aguarda a resposta]</ListItem>
+          <ListItem>
+            Percebeu a diferença? Quando eu pergunto em inglês, você
+            automaticamente traduz. A Wise Up vai te fazer pensar em inglês. Pra
+            quando eu perguntar o que é yellow, vc já me dizer que é uma cor,
+            sem precisar ficar traduzindo. Por isso que o seu aprendizado vai
+            ser de uma forma muito mais objetiva.
+          </ListItem>
+          <ListItem>
+            Deu pra entender como funciona essa parte? [Aguarda a resposta]
+          </ListItem>
+          <ListItem>
+            Além disso, você vai ter o acompanhamento do seu aplicativo, como eu
+            te falei ele é super moderno, inspirado na Netflix e ali você tem
+            exercícios de fixação, tem o material didático pra você baixar, ele
+            é um aplicativo bem completo para complementar os seus estudos.
+          </ListItem>
+          <ListItem>
+            xxxx, é basicamente isso a metodologia. O que você achou do nosso
+            curso?
+          </ListItem>
+          <ListItem>
+            xxxx, se dependesse só do método, sem contar o valor, você se
+            tornaria aluno(a) da Wise Up? [Aguarda a resposta]
+          </ListItem>
+        </List>
+      </section>
+      <section>
+        <Heading>Fechamento</Heading>
+        <List>
+          <ListItem>
+            Pelo método de ensino, você se tornaria nosso aluno?
+          </ListItem>
+          <ListItem>
+            Fulano, existem vários tipos de escola.. Do nível A ao nível C… E
+            Como são feitas as classificações? Líderes do mercado, e tempo que o
+            aluno demora pra aprender
+          </ListItem>
+          <ListItem>
+            Então uma escola de nível A o aluno vai ficar de 18 a 24 meses. Ele
+            vai investir um valor mais alto por mês.. Se multiplicar pelo tempo
+            que vai ficar, vai sair bem mais acessível que uma escola de nível C
+          </ListItem>
+          <ListItem>
+            Então quem são as escolas de nível A? Allumi, Berlitz, Cel Lep, Wise
+            Up.. E o aluno ele vai estudar de 18 a 24 meses e investir em torno
+            de 700 a 900 reais por mês, beleza?
+          </ListItem>
+          <ListItem>
+            Escolas de nível B.. O aluno fica de 4 a 5 anos.. São escolas como:
+            Cultura Inglesa, União Cultural e Senac.. O aluno ele vai investir
+            em torno de 450 a 600 reais por mês
+          </ListItem>
+          <ListItem>
+            E as escolas de nível C.. São escolas como: Wizard, Fisk, Microlins,
+            CCA e Skill.. São escolas populares, onde o aluno vai investir em
+            torno de 350 a 400 reais por mês
+          </ListItem>
+          <ListItem>
+            Lembra que eu te falei? É mais barato, mas o tempo que o aluno vai
+            ficar, vai sair mais caro
+          </ListItem>
+          <ListItem>
+            Então vamos la, agora, o que eu quero entender.. Qual a tua
+            necessidade com o Inglês? Ficar mais tempo, menos tempo? O que você
+            busca hoje?
+          </ListItem>
+          <ListItem>
+            Tá, agora que que eu quero pensar.. Que é um investimento pro seu
+            futuro.. Investimento é o que sobra.. Acredito que você não tem
+            1.000 reais sobrando pra fazer o Inglês, né fulano?
+          </ListItem>
+          <ListItem>
+            Então, mas é um investimento, é o que vc se aperta.. Em vista de
+            tudo que vc me falou no começo, que o Inglês ia mudar sua vida..
+            Então hoje você se apertando assim, sugando ao máximo, em qual
+            escola vc consegue investir? Nível A, de 700 a 900.. Nível B, de 450
+            a 600.. Ou nível C de 350 a 400 reais? (Espera resposta)
+          </ListItem>
+          <ListItem>
+            Então tá fulano, pra eu entender.. Vc tá falando que quer estudar em
+            uma escola de nível A, com valor de nível C? Tipo assim. Quer
+            comprar uma Ferrari, com o valor de um Fusca? (Risada)
+          </ListItem>
+          <ListItem>
+            Beleza, eu vou ver o que consigo fazer pra te ajudar.. Mas tenta se
+            apertar ai um pouquinho.. Abrindo mão do valor da pizza no final de
+            semana.. Vc consegue investir um pouquinho mais, uns 450 reais,
+            apertando muito?
+          </ListItem>
+          <ListItem>
+            Pq eu to te falando isso? Qual é o valor hoje regular da Wise Up?
+            Sem esse projeto, com nada disso
+          </ListItem>
+          <ListItem>
+            É uma matrícula única, que o aluno ele paga 600 reais.. Essa
+            matrícula ela não sofre reajuste ta? Ela é única pro curso inteiro..
+            Então não vai ter aquela rematrícula daqui 6 meses, 1 ano.. É
+            matrícula única
+          </ListItem>
+          <ListItem>
+            Mensalidade, vc vai investir 500, 600 reais.. Valor fixo, sem
+            reajuste.. E o material, vc investe 12 parcelas de 198.. Então o
+            Ticket médio pra você estudar na nossa escola, é de 698 reais por
+            mês, ta?
+          </ListItem>
+          <ListItem>
+            Esse valor, pelo que eu entendi, te aperta hoje né?
+          </ListItem>
+          <ListItem>
+            Ta, então pra vaga de bolsa tem só dois pré requisitos, beleza?
+          </ListItem>
+          <ListItem>
+            Primeiro: Ética no sigilo de valor: Não contar pra ninguém o valor
+            que você vai investir.. Pq? A maioria dos meus alunos são alunos
+            regulares.. Se vc é um aluno bolsista, e abre o valor, dá um atrito
+            danado dentro da sala de aula.. Então posso contar com a sua ética e
+            sigilo de valor?
+          </ListItem>
+          <ListItem>
+            Segundo: Empenho e dedicação.. Pq vc vai investir no seu futuro, mas
+            a Wise Up também vai pagar uma parte do seu curso, por conta da
+            bolsa.. Então posso contar que você vai entrar pra realmente
+            aprender e se dedicar?
+          </ListItem>
+          <ListItem>
+            Então só um minutinho que vou abrir o sistema pra ver o valor que ta
+            disponível pra vc (Da um tempinho)
+          </ListItem>
+          <ListItem>
+            Show, ok. Eu vi aqui, e o valor que tá disponível no sistema, e esse
+            é O MENOR valor ta? Ta uma matrícula ÚNICA, vc nao vai pagar
+            rematricula, ta 250 reais.. Mensalidade vai ficar só 198 reais..
+            Esse valor ele é fixo pro curso todo, então não sofre reajuste.. E o
+            material vc investe ai 151 reais por mês durante o tempo do curso
+          </ListItem>
+          <ListItem>
+            Esse valor, pelo que você me falou, cabe dentro da tua realidade
+            econômica, né fulano?
+          </ListItem>
+          <ListItem>
+            Legal, então agora é bem simples, ta fulano? Pra gente fazer tua
+            inscrição, eu já vou passar o teu material e sua taxa de matricula
+          </ListItem>
+          <ListItem>
+            Então mensalidade e a segunda parcela do material, vc só paga um mês
+            após.. Então por exemplo, vc começa agora em março, e só começa a
+            pagar em abril, beleza?
+          </ListItem>
+        </List>
+      </section>
+    </Stack>
+  )
+}
--- src/theme.tsx
+++ src/theme.tsx
@@ -1,39 +1,27 @@
-import { extendTheme } from '@chakra-ui/react'
-
-const fonts = { mono: `'Menlo', monospace` }
-
-const breakpoints = {
-  sm: '40em',
-  md: '52em',
-  lg: '64em',
-  xl: '80em',
-}
+import { extendTheme } from "@chakra-ui/react"
 
 const theme = extendTheme({
-  semanticTokens: {
-    colors: {
-      text: {
-        default: '#16161D',
-        _dark: '#ade3b8',
-      },
-      heroGradientStart: {
-        default: '#7928CA',
-        _dark: '#e3a7f9',
-      },
-      heroGradientEnd: {
-        default: '#FF0080',
-        _dark: '#fbec8f',
-      },
-    },
-    radii: {
-      button: '12px',
-    },
-  },
   colors: {
-    black: '#16161D',
+    black: "#16161D",
   },
-  fonts,
-  breakpoints,
+  fonts: {
+    body: `'Poppins', sans-serif`,
+    heading: `'Poppins', sans-serif`,
+  },
+  breakpoints: {
+    sm: "40em",
+    md: "52em",
+    lg: "64em",
+    xl: "80em",
+  },
+  styles: {
+    global: {
+      "hmtl, body": {
+        bg: "#222",
+        color: "#fff",
+      },
+    },
+  },
 })
 
 export default theme
